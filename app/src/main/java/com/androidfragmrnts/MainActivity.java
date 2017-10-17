81c8a662085f613849cede00f660320c6ff9d19f
DevasatSandeepKumar <devasat.sandeepkumar@gmail.com>
2017-10-17 15:16:41 +0530

    Android Fragments

---
+++ app/src/main/java/com/androidfragmrnts/MainActivity.java
@@ -0,0 +1,17 @@
+package com.androidfragmrnts;
+
+/**
+ * Created by Sandeep on 17/10/17.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
